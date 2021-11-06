b1105a8616b7c86f48c8ba9549e7166f72f31c2c
danielbsoftdev <93794013+danielbsoftdev@users.noreply.github.com>
2021-11-06 10:23:42 +0200

    Added classes to separate files

---
+++ Skill.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Test
+{
+    public class Skill
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsAttackSkill { get; set; }
+        public int UseLikehood { get; set; }
+        public List<int> TurnsWhenSkillApplies { get; set; }
+        public Skill(string name, string description, bool isAttackSkill, int useLikehood)
+        {
+            Name = name;
+            Description = description;
+            IsAttackSkill = isAttackSkill;
+            UseLikehood = useLikehood;
+            TurnsWhenSkillApplies = Program.GetTurns(useLikehood, true);
+        }
+    }
+}
